5afa6596a4f07ee840cd64c5051bdd5272eeb3cc
dantesantos <46685889+dantesantos@users.noreply.github.com>
2019-02-11 22:08:13 -0500

    Add project files.

---
+++ BibleStudyGuide/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BibleStudyGuide.MvcApplication" Language="C#" %>
---
+++ BibleStudyGuide/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BibleStudyGuide.Startup))]
+namespace BibleStudyGuide
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
